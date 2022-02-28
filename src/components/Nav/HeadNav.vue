12c5bcb83ba774d47225c3ca656ce3f601502ec1
Amadej Krepek <59646470+AmadejKrepek@users.noreply.github.com>
2022-02-28 09:49:16 +0100

    Renamed to head navigation component

--- src/components/Nav/HeadNav.vue
+++ src/components/Nav/HeadNav.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <router-link to="/">Dashboard</router-link> |
-    <router-link to="/about">About</router-link>
+    <ul class="nav justify-content-center bg-dark text-white p-2">
+      <li class="nav-item">
+        <router-link class="nav-link active" to="/">Dashboard</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/about">Data</router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HeadNav',
-}
+  name: "HeadNav",
+};
 </script>
