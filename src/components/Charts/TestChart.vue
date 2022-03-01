333adb3f6acad72173d0d8592ff42e28a138356a
Amadej Krepek <59646470+AmadejKrepek@users.noreply.github.com>
2022-03-01 20:53:57 +0100

    Added simple highcharts data

--- src/components/Charts/TestChart.vue
+++ src/components/Charts/TestChart.vue
@@ -1,5 +1,11 @@
 <template>
-  <Highcharts v-if="GetChartOptions != null" ref="highchartsRef" :options="GetChartOptions" />
+  <div v-if="GetChartOptions != null">
+    <button class="btn btn-primary">Altitude</button>
+    <button class="btn btn-primary">Distance</button>
+    <button class="btn btn-primary">Heart Rate</button>
+    <button class="btn btn-primary">Speed</button>
+    <Highcharts ref="highchart" :options="item.chartOptions" v-for="item in GetChartOptions" :key="item" />
+  </div>
 </template>
 
 <script>
